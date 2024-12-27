09a1c17233355c903895dcbdf582ee7f35061558
harbissr <harbissr@gmail.com>
2024-12-27 15:25:09 -0500

    region app serializers and urls made

--- region_app/views.py
+++ region_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Country, State, City
+from .serializers import CountrySerializer, StateSerializer, CitySerializer
 
-# Create your views here.
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class StateViewSet(viewsets.ModelViewSet):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
