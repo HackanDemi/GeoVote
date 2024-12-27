60db404e774b3cdea622cc99613cc2eb18c9938e
HakanDemirbas <demirbashakan54@gmail.com>
2024-12-27 10:45:14 -0600

    Add MapTiler component

---
+++ frontend/src/components/MapTiler.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MapTiler() {
+    // hook
+    // API key should be .env file
+
+
+    // useEffect with async axios fetch
+
+  
+    return (
+    <div>MapTiler</div>
+  )
+}
