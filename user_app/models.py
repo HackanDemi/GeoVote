cb3d11da3144fcec48a7a33f3607007a45579da2
harbissr <harbissr@gmail.com>
2024-12-27 12:45:23 -0500

    region app models made and changes to user app models for migrations

--- user_app/models.py
+++ user_app/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+# class User(AbstractUser):
+#     email = models.EmailField(unique=True, blank=False)
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
+
+
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False)
-    
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    groups = models.ManyToManyField(
+        Group, related_name="custom_user_set", blank=True  # Change to a unique name
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # Change to a unique name
+        blank=True,
+    )
