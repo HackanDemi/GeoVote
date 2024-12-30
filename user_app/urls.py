d7cb445944a6776416749de99950c6a434265e99
harbissr <harbissr@gmail.com>
2024-12-30 17:36:36 -0500

    changes made for merge

--- user_app/urls.py
+++ user_app/urls.py
@@ -3,8 +3,7 @@ from .views import SignUp, LogIn, LogOut
 
 
 urlpatterns = [
-    path('signup/', SignUp.as_view(), name='signup'),
-    path('login/', LogIn.as_view(), name='login'),
-    path('logout/', LogOut.as_view(), name='logout'),
+    path("signup/", SignUp.as_view(), name="signup"),
+    path("login/", LogIn.as_view(), name="login"),
+    path("logout/", LogOut.as_view(), name="logout"),
 ]
-
