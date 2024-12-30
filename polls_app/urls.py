550f5d44df5ee2acd3e4ac32cd68b50dc116e357
harbissr <140867776+harbissr@users.noreply.github.com>
2024-12-30 17:54:29 -0500

    deleted comments from urls.py

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# app_name = "polls_app"
-# urlpatterns = [
-#     path("", views.index, name="index"),
-#     path("<int:question_id>/", views.detail, name="detail"),
-#     path("<int:question_id>/results/", views.results, name="results"),
-# ]
-
 from django.urls import path
 from .views import CreatePollView
 
