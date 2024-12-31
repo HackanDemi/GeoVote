07f72189f7bea64feaf9ad9e42e3ffb4528295ea
HakanDemirbas <demirbashakan54@gmail.com>
2024-12-31 10:02:03 -0600

    Create MapTiler functionality

--- frontend/src/components/MapTiler.jsx
+++ frontend/src/components/MapTiler.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-// Need SDK JS library
+import React, { useEffect } from 'react';
+import maptiler from '@maptiler/sdk';
+import axios from 'axios';
 
 export default function MapTiler() {
-    // hook
-    // API key should be .env file
+    useEffect(() => {
+        const fetchData = async () => {
+            const apiKey = process.env.REACT_APP_MAPTILER_API_KEY;
+            const response = await axios.get(`https://api.maptiler.com/maps/basic-v2/style.json?key=${apiKey}`);
+            console.log(response.data);
+        };
 
+        fetchData();
+    }, []);
 
-    // useEffect with async axios fetch
-
-  
     return (
-    <div>MapTiler</div>
-  )
+        <div>MapTiler</div>
+    );
 }
