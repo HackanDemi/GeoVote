a4f272bcb1087ee0e9e4fb76d9ff245db5b5b421
HakanDemirbas <demirbashakan54@gmail.com>
2025-01-01 19:53:55 -0600

    Fix some routes and component imports

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Map from '../components/MapTiler/MapTiler'
 
 export default function HomePage() {
   return (
     <>
     <NavBar />
+    <Map />
     <div>HomePage</div>
     </>
   )
