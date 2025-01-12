edbce4158ef7c1dc6ec8d8946dbcb5a2a1d5cb1c
harbissr <harbissr@gmail.com>
2025-01-11 19:11:55 -0500

    added city and state back to model

--- polls_app/models.py
+++ polls_app/models.py
@@ -5,6 +5,8 @@ from region_app.models import City, State
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField("date published")
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
 
