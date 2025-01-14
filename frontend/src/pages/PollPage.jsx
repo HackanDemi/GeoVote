b53eff661f11108fea07d76e5b57c6431b6740ec
demi <h.demirbas@bssdstd.net>
2025-01-14 20:59:35 +0000

    Update PollPage.jsx

--- frontend/src/pages/PollPage.jsx
+++ frontend/src/pages/PollPage.jsx
@@ -1,33 +1,16 @@
-import { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar'
-// import PizzaPoll from '../components/PizzaPoll'
-import PollCard from '../components/PollsCard';
-import { getAllPolls } from '../utilities';
+import Poll from '../components/Poll'
+
 
 export default function PollPage() {
-  const [polls, setPolls] = useState([]);
-
-  useEffect(() => {
-    const getPolls = async() => {
-      const usersPolls = await getAllPolls();
-      if(usersPolls) {
-        setPolls(usersPolls);
-      }
-    };
-    getPolls();
-  }, []);
-
   return (
     <>
-      <NavBar />
-      <div style={{ width: '100vw' }}>
-        {polls.map(poll => (
-          <PollCard
-            key={poll.id}
-            edit
-          />
-        ))}
-      </div>
+    <NavBar />
+    <div className="page-container">
+      <Poll />
+    </div>
     </>
-  );
-};
+  )
+}
+
+
